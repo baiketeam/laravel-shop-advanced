c3b201d88a57f4f7997d543b297a0f9793a2e12b
张汉为 <33750720@qq.com>
2018-10-18 02:42:43 +0000

    收货地址列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function addresses()
+    {
+        return $this->hasMany(UserAddress::class);
+    }
 }
