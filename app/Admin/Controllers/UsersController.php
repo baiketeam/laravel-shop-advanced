b72f480ad5c375bd61e0d9de5c81585219467472
张汉为 <33750720@qq.com>
2018-10-18 04:43:55 +0000

    管理后台-用户列表

---
+++ app/Admin/Controllers/UsersController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class UsersController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('用户列表')
+            ->description('description')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new User);
+
+        $grid->id('Id');
+        $grid->name('姓名');
+        $grid->email('Email');
+        $grid->email_verified_at('是否已验证邮箱')->display(function($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->created_at('注册时间');
+        $grid->disableCreateButton(); // 禁用新增按钮
+        $grid->actions(function ($actions) {
+            // 不在每一行后面展示查看按钮
+            $actions->disableView();
+
+            // 不在每一行后面展示删除按钮
+            $actions->disableDelete();
+
+            // 不在每一行后面展示编辑按钮
+            $actions->disableEdit();
+        });
+        $grid->tools(function ($tools) {
+            // 禁用批量刪除按鈕
+            $tools->batch(function ($batch) {
+                $batch->disableDelete();
+            });
+        });
+        // $grid->password('Password');
+        // $grid->remember_token('Remember token');
+        // $grid->email_verified('Email verified');
+        // $grid->created_at('Created at');
+        // $grid->updated_at('Updated at');
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(User::findOrFail($id));
+
+        $show->id('Id');
+        $show->name('Name');
+        $show->email('Email');
+        $show->email_verified_at('Email verified at');
+        $show->password('Password');
+        $show->remember_token('Remember token');
+        $show->email_verified('Email verified');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new User);
+
+        $form->text('name', 'Name');
+        $form->email('email', 'Email');
+        $form->datetime('email_verified_at', 'Email verified at')->default(date('Y-m-d H:i:s'));
+        $form->password('password', 'Password');
+        $form->text('remember_token', 'Remember token');
+        $form->switch('email_verified', 'Email verified');
+
+        return $form;
+    }
+}
