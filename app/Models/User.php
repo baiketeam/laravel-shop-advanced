574eaa951469cc946b2eb57cd22fd56c81a921b0
张汉为 <33750720@qq.com>
2018-10-18 01:52:00 +0000

    邮箱验证中间件

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -16,7 +16,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'email_verified',
+    ];
+
+    protected $casts = [
+        'email_verified' => 'boolean',
     ];
 
     /**
