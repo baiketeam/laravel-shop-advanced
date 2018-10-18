e4c20a96aac82fd63ece034c285b47981ab92acb
张汉为 <33750720@qq.com>
2018-10-18 03:03:27 +0000

    新增收货地址

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,8 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-
+require('./components/SelectDistrict');
+require('./components/UserAddressesCreateAndEdit');
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
