360d0a0dc20c499009c9c5561806246742564b13
张汉为 <33750720@qq.com>
2018-10-19 06:08:32 +0000

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
 
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
@@ -34,7 +34,10 @@ Route::group(['middleware' => 'auth'], function() {
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
     });
 });
+
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 
