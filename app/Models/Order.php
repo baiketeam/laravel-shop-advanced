ddb811c6dab70777b03c7ab040ad9326b6c79d31
张汉为 <33750720@qq.com>
2018-10-19 13:55:51 +0000

    创建订单

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -61,14 +61,14 @@ class Order extends Model
 
     protected static function boot()
     {
-        parent::boot();
-        // 监听模型创建时间,在写入数据库之前触发
+       parent::boot();
+        // 监听模型创建事件，在写入数据库之前触发
         static::creating(function ($model) {
-            // 如果模型的no字段为空
+            // 如果模型的 no 字段为空
             if (!$model->no) {
-                // 调用findAvailableNo生成订单流水号
+                // 调用 findAvailableNo 生成订单流水号
                 $model->no = static::findAvailableNo();
-                // 如果生成失败则终止创建订单
+                // 如果生成失败，则终止创建订单
                 if (!$model->no) {
                     return false;
                 }
@@ -91,7 +91,7 @@ class Order extends Model
         // 订单流水号前缀
         $prefix = date('YmdHis');
         for ($i=0; $i<10; $i++) {
-            $no = $prefix.str_pad(random(0,999999),6,'0',STR_PAD_LEFT);
+            $no = $prefix.str_pad(random_int(0, 999999), 6, '0', STR_PAD_LEFT);
             // 判断是否已经存在
             if (!static::query()->where('no', $no)->exists()) {
                 return $no;
