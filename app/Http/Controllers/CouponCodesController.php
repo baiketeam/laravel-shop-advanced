b1966e6b6c7f11ac3ba0c82e1568490cf50005a1
张汉为 <33750720@qq.com>
2018-10-23 05:11:02 +0000

    用户界面 - 使用优惠券下单

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -6,32 +6,38 @@ use Illuminate\Http\Request;
 
 use App\Models\CouponCode;
 use Carbon\Carbon;
+use App\Exceptions\CouponCodeUnavailableException;
 
 class CouponCodesController extends Controller
 {
     public function show($code)
     {
-        // 判断优惠券是否存在
+        // // 判断优惠券是否存在
+        // if (!$record = CouponCode::where('code', $code)->first()) {
+        //     abort(404);
+        // }
+
+        // // 如果优惠券没有启用则等同于没有优惠券
+        // if (!$record->enabled) {
+        //     abort(404);
+        // }
+
+        // if ($record->total - $record->used <= 0) {
+        //     return response()->json(['msg' => '优惠券已经被兑完'], 403);
+        // }
+
+        // if ($record->not_before && $record->not_before->gt(Carbon::now())) {
+        //     return response()->json(['msg' => '该优惠券现在还不能使用'], 403);
+        // }
+
+        // if ($record->not_after && $record->not_after->lt(Carbon::now())) {
+        //     return response()->json(['msg' => '该优惠券已过期'], 403);
+        // }
         if (!$record = CouponCode::where('code', $code)->first()) {
-            abort(404);
+            throw new CouponCodeUnavailableException('优惠券不存在');
         }
 
-        // 如果优惠券没有启用则等同于没有优惠券
-        if (!$record->enabled) {
-            abort(404);
-        }
-
-        if ($record->total - $record->used <= 0) {
-            return response()->json(['msg' => '优惠券已经被兑完'], 403);
-        }
-
-        if ($record->not_before && $record->not_before->gt(Carbon::now())) {
-            return response()->json(['msg' => '该优惠券现在还不能使用'], 403);
-        }
-
-        if ($record->not_after && $record->not_after->lt(Carbon::now())) {
-            return response()->json(['msg' => '该优惠券已过期'], 403);
-        }
+        $record->checkAvailable();
 
         return $record; 
     }
