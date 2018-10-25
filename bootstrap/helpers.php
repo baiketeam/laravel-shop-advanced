afe57e48083f5cb87cda416f35f6bb985c3cb37c
张汉为 <33750720@qq.com>
2018-10-25 12:35:35 +0000

    初始化分期付款模型

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,6 +5,7 @@ function route_class()
     return str_replace('.', '-', Route::currentRouteName());
 }
 
+// 开发环境外网访问配置
 function ngrok_url($routeName, $parameters = [])
 {
     // 开发环境，并且配置了 NGROK_URL
@@ -14,4 +15,11 @@ function ngrok_url($routeName, $parameters = [])
     }
 
     return route($routeName, $parameters);
+}
+
+
+// 默认的精度为小数点后两位
+function big_number($number, $scale = 2)
+{
+    return new \Moontoast\Math\BigNumber($number, $scale);
 }
