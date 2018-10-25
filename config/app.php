af936bed11078fc0179178c7f8fc77ecce6ea74e
张汉为 <33750720@qq.com>
2018-10-25 13:20:09 +0000

    创建分期付款

--- config/app.php
+++ config/app.php
@@ -20,7 +20,17 @@ return [
 
     'order_ttl' => 1800,
 
+    // 生产环境外网访问配置
     'ngrok_url' => env('NGROK_URL'),
+
+    // 分期付款设置
+    'installment_fee_rate' => [
+        3  => 1.5, 
+        6  => 2,
+        12 => 2.5,
+    ], // 分期费率，key 为期数，value 为费率
+    'min_installment_amount' => 300, // 最低分期金额
+    'installment_fine_rate' => 0.05, // 逾期日息 0.05%
 
     /*
     |--------------------------------------------------------------------------
