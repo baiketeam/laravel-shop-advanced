3e6cf148e331287720f6596e038fe3e5c2255fb0
张汉为 <33750720@qq.com>
2018-10-31 04:27:49 +0000

    减少秒杀接口 SQL 查询

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use Monolog\Logger;
 use Yansongda\Pay\Pay;
 use Carbon\Carbon;
 use Elasticsearch\ClientBuilder as ESClientBuilder;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
         // 同时 Laravel 还支持通配符，例如 products.* 即代表当渲染 products 目录下的模板时都执行这个 ViewComposer
         \View::composer(['products.index', 'products.show'], \App\Http\ViewComposers\CategoryTreeComposer::class); 
         Carbon::setLocale('zh');
+        // 只在本地开发环境启用 SQL 日志
+        if (app()->environment('local')) {
+            \DB::listen(function ($query) {
+                \Log::info(Str::replaceArray('?', $query->bindings, $query->sql));
+            });
+        }
     }
 
     /**
--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -192,18 +192,18 @@ class OrderService
         }
     }
 
-    public function seckill(User $user, UserAddress $address, ProductSku $sku)
+    public function seckill(User $user, array $addressData, ProductSku $sku)
     {
-        $order = \DB::transaction(function () use ($user, $address, $sku) {
+        $order = \DB::transaction(function () use ($user, $addressData, $sku) {
             // 更新此地址的最后使用时间
-            $address->update(['last_used_at' => Carbon::now()]);
+            // $address->update(['last_used_at' => Carbon::now()]);
             // 创建一个订单
             $order = new Order([
                 'address'      => [ // 将地址信息放入订单中
-                    'address'       => $address->full_address,
-                    'zip'           => $address->zip,
-                    'contact_name'  => $address->contact_name,
-                    'contact_phone' => $address->contact_phone,
+                    'address'       => $addressData['province'].$addressData['city'].$addressData['district'].$addressData['address'],
+                    'zip'           => $addressData['zip'],
+                    'contact_name'  => $addressData['contact_name'],
+                    'contact_phone' => $addressData['contact_phone'],
                 ],
                 'remark'       => '',
                 'total_amount' => $sku->price,
